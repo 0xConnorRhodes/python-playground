7c77b608c3abcf7b497172d759b814dde7fd8eb8
Connor Rhodes <connor@rhodes.contact>
2022-11-18 14:11:32 -0600

    added variable unpacking experiment

---
+++ acg.introduction-to-python-development/experiments/variable-unpacking.py
@@ -0,0 +1,20 @@
+mylist = [1, 2, 3]
+
+
+print(mylist)
+
+a, b, c = mylist
+
+print(a)
+print(b)
+print(c)
+
+mytuple = (4, 5, 6)
+
+print(mytuple)
+
+d, e, f = mytuple
+
+print(d)
+print(e)
+print(f)
