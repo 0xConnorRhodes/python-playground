b79414efb70732016271dddbc8be1b127f46929c
Connor Rhodes <connor@rhodes.contact>
2022-11-20 10:41:47 -0600

    added initial exercise code

---
+++ 100-days-of-python/day-2-datq-types-and-string-manipulation/exercise-1-data-types.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
