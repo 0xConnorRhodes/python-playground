6e51a2132980203f09353c0151f6611fa0800f84
Connor Rhodes <connor@rhodes.contact>
2022-11-23 16:12:33 -0600

    added tests

--- introduction-to-python-scripting/assignments/2-mathematical-operations.py
+++ introduction-to-python-scripting/assignments/2-mathematical-operations.py
@@ -13,3 +13,17 @@ def divide(num1, num2):
     if num2 == 0:
         return "Error: Cannot divide by 0"
     return num1 / num2
+
+num1 = 10
+num2 = 2
+
+print(add(num1, num2))
+
+print(subtract(num1, num2))
+
+print(multiply(num1, num2))
+
+print(divide(num1, num2))
+
+num2 = 0
+print(divide(num1, num2))
