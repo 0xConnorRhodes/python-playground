8cfe4dcdd181e47fdad43045ffa508131b0e27da
Connor Rhodes <connor@rhodes.contact>
2022-11-23 11:35:47 -0600

    fixed shape of top pyramid

--- introduction-to-python-scripting/assignments/1-ascii-diamond.py
+++ introduction-to-python-scripting/assignments/1-ascii-diamond.py
@@ -4,7 +4,7 @@ length = 10
 astr = "*"
 
 for i in range(length):
-    for j in range(length):
+    for j in range(length-i):
         print("-", end='')
     print(astr)
     astr = astr + "**"
@@ -13,5 +13,6 @@ length -= 1
 astrNum = len(astr) - 4
 
 for i in range(length):
+    print("-", end='')
     print("*" * astrNum)
     astrNum -= 2
