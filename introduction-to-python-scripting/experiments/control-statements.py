824df1bee371006f3b4905bd77635455a53a7415
Connor Rhodes <connor@rhodes.contact>
2022-11-22 20:57:16 -0600

    added 3 control statements

--- introduction-to-python-scripting/experiments/control-statements.py
+++ introduction-to-python-scripting/experiments/control-statements.py
@@ -1,5 +1,32 @@
 #!/usr/bin/env python3
 
+# break
 for num in range(1,6):
+    if num > 3:
+        break
     print(num)
 
+    if num == 4: # skipped
+        print("It's four") # skipped
+
+print("break done") # executed
+
+# continue
+for num in range(1,6):
+
+    if num == 3:
+        continue # skips printing 3
+
+    print(num)
+
+print("continue done")
+
+# pass
+for num in range(1,6):
+
+    if num == 3:
+        pass # pass allows the empty if to execute
+
+    print(num)
+
+print("pass done")
