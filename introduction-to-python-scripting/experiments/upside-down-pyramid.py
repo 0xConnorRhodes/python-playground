3926a30f8e2adfd65f8b85cfdcc7e573d1f8c945
Connor Rhodes <connor@rhodes.contact>
2022-11-23 12:04:29 -0600

    working solution

--- introduction-to-python-scripting/experiments/upside-down-pyramid.py
+++ introduction-to-python-scripting/experiments/upside-down-pyramid.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 
-length
+length = 11
+space = 1
+count = 0
+astr = "*" * length
+
+for i in range(length):
+    for i in range(i):
+        print(" ", end='')
+    print(astr)
+    astr = "*" * (len(astr) - 2 )
+    if len(astr) < 1:
+        break
