f9804c1ad3bf889a8aaa4d75b8fe6af02afda594
Connor Rhodes <connor@rhodes.contact>
2022-11-23 16:10:32 -0600

    completed assignment

--- introduction-to-python-scripting/assignments/2-mathematical-operations.py
+++ introduction-to-python-scripting/assignments/2-mathematical-operations.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    if num2 == 0:
+        return "Error: Cannot divide by 0"
+    return num1 / num2
