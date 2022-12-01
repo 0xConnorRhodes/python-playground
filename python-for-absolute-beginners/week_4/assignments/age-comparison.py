b208d051615a4b73bdd62dee977bdcbf94dd61c4
Connor Rhodes <connor@rhodes.contact>
2022-11-30 20:25:46 -0600

    completed

--- python-for-absolute-beginners/week_4/assignments/age-comparison.py
+++ python-for-absolute-beginners/week_4/assignments/age-comparison.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+
+my_age = 26
+
+your_name = input("What is your name? ")
+your_age = int(input("What is your age? "))
+
+print(f"Hello {your_name}. I am {my_age - your_age} years older than you.")
