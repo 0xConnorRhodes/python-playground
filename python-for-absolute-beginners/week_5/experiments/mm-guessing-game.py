205bbeb651bd2356bdfd2e2f9e61effb8a794318
Connor Rhodes <connor@rhodes.contact>
2022-12-02 12:19:38 -0600

    added outline of game functionality

---
+++ python-for-absolute-beginners/week_5/experiments/mm-guessing-game.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""
+1. choose random number for MMs in the jar
+2. give the user 5 attempts to guess the correct number of MMs
+3. tell the user if their guess is too low or 2 high
+4. if the user guesses correctly, exit immediately and tell them the number of guesses it took them to guess correctly
+"""
