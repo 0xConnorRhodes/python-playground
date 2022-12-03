de4c9478e7d83fcb7f06460a62e90c990b37baf2
Connor Rhodes <connor@rhodes.contact>
2022-12-03 12:16:48 -0600

    completed continuous odd/even

--- python-for-absolute-beginners/week_5/assignments/3-continuous-odd-even.py
+++ python-for-absolute-beginners/week_5/assignments/3-continuous-odd-even.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
-num = int(input("Please choose a number "))
+while True:
+    num = int(input("Please choose a number "))
 
-if num % 2 == 1:
-    print("The number is odd.")
-else:
-    print("The number is even.")
+    if num == 0:
+        print("Goodbye")
+        break
+    elif num % 2 == 1:
+        print("The number is odd.")
+    else:
+        print("The number is even.")
