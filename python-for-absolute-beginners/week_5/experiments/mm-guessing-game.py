796ee8bf3d025b1ec29978cf06a7a4d3b0ef06db
Connor Rhodes <connor@rhodes.contact>
2022-12-03 18:34:08 -0600

    added random number generator

--- python-for-absolute-beginners/week_5/experiments/mm-guessing-game.py
+++ python-for-absolute-beginners/week_5/experiments/mm-guessing-game.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
+import random
+
 """
 1. choose random number for MMs in the jar
 2. give the user 5 attempts to guess the correct number of MMs
 3. tell the user if their guess is too low or 2 high
 4. if the user guesses correctly, exit immediately and tell them the number of guesses it took them to guess correctly
 """
+num = random.randint(1, 101)
+
+print(num)
