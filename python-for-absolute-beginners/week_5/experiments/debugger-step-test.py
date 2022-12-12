0353592f3b0631f5f74369b13568670c70596a49
Connor Rhodes <connor@rhodes.contact>
2022-12-12 11:39:08 -0600

    added comments to debugger step test

--- python-for-absolute-beginners/week_5/experiments/debugger-step-test.py
+++ python-for-absolute-beginners/week_5/experiments/debugger-step-test.py
@@ -1,8 +1,14 @@
-
 def function1():
     print("function 1 step 1")
     print("function 1 step 2")
 
+
 def function2():
     print("function 2 step 1")
     print("function 2 step 2")
+
+function1() # step over interprets this as one step
+
+function1() # step into interprets this as two steps
+
+function2()
