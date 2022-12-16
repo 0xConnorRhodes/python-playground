7503acb5f80705021e443decdd58d188770cc28f
Connor Rhodes <connor@rhodes.contact>
2022-12-15 21:42:36 -0600

    organized into functions

--- python-for-absolute-beginners/week_6/assignment/organize-code-with-functions.py
+++ python-for-absolute-beginners/week_6/assignment/organize-code-with-functions.py
@@ -1,27 +1,42 @@
 import random
 
-print("------------------------------")
-print("     M&M guessing game!")
-print("------------------------------")
 
-print("Guess the number of M&Ms and you get lunch on the house!")
-print()
+def main():
+    print_intro()
+    play_game()
+    say_goodbye()
 
-mm_count = random.randint(1, 100)
-attempt_limit = 5
-attempts = 0
 
-while attempts < attempt_limit:
-    guess_text = input("How many M&Ms are in the jar? ")
-    guess = int(guess_text)
-    attempts += 1
+def print_intro():
+    print("------------------------------")
+    print("     M&M guessing game!")
+    print("------------------------------")
 
-    if mm_count == guess:
-        print(f"You got a free lunch! It was {guess}.")
-        break
-    elif guess < mm_count:
-        print("Sorry, that's too LOW!")
-    else:
-        print("That's too HIGH!")
+    print("Guess the number of M&Ms and you get lunch on the house!")
+    print()
 
-print(f"Bye, you're done in {attempts}!")
+
+def play_game():
+    mm_count = random.randint(1, 100)
+    attempt_limit = 5
+    attempts = 0
+
+    while attempts < attempt_limit:
+        guess_text = input("How many M&Ms are in the jar? ")
+        guess = int(guess_text)
+        attempts += 1
+
+        if mm_count == guess:
+            print(f"You got a free lunch! It was {guess}.")
+            break
+        elif guess < mm_count:
+            print("Sorry, that's too LOW!")
+        else:
+            print("That's too HIGH!")
+
+
+def say_goodbye():
+    print(f"Bye, you're done in {attempts}!")
+
+
+main()
