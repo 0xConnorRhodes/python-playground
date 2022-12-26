7e0e31f697bd64fe00ddb60fddd6cd969f15842e
Connor Rhodes <connor@rhodes.contact>
2022-12-26 11:37:03 -0600

    further tested whitespace inside parenthesis

--- python-for-non-programmers/2_if-statements/1_experiments/parenthesis-and-multi-line-comparisons.py
+++ python-for-non-programmers/2_if-statements/1_experiments/parenthesis-and-multi-line-comparisons.py
@@ -4,7 +4,9 @@ x = 10
 
 if (x == 10 # inside parenthesis, python does not care about whitespace
        or x == 15 or
-x == 20):
+x 
+== 
+20):
     print("It is 10 or 15 or 20.")
 else:
     print("It's another number")
