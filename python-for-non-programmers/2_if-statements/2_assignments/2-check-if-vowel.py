5c62f7cf7115718da99c45cc99b273ac03521a14
Connor Rhodes <connor@rhodes.contact>
2022-12-26 17:48:06 -0600

    completed assignment

---
+++ python-for-non-programmers/2_if-statements/2_assignments/2-check-if-vowel.py
@@ -0,0 +1,10 @@
+letter = input("Please enter a letter: ")
+
+if (letter == 'a' or
+    letter == 'e' or
+    letter == 'i' or
+    letter == 'o' or
+    letter == 'u'):
+    print(f"{letter} is a vowel.")
+else:
+    print(f"{letter} is a consonant.")
