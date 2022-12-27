c9024b94fb457335855b81443d16b15f651dab0b
Connor Rhodes <connor@rhodes.contact>
2022-12-27 17:01:23 -0600

    completed calculator.py

--- python-for-non-programmers/3_numbers/1_experiments/calculator.py
+++ python-for-non-programmers/3_numbers/1_experiments/calculator.py
@@ -3,3 +3,14 @@
 # (3) Ask the user to enter "operation, either '+' or '*'
 # (4) Print either x+y or x*y, depending on the operation
 
+num1 = int(input("Enter a numer: "))
+num2 = int(input("Enter another numer: "))
+
+operation = input("Choose '+' or '*': ")
+
+if operation == "+":
+    print(f"{num1} + {num2} is {num1 + num2}")
+elif operation == "*":
+    print(f"{num1} * {num2} is {num1 * num2}")
+else:
+    print("that operation is invalid")
