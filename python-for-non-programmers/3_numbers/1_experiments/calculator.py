682ca65ebd6a3b9b43ef4f9ab2a4b75b5dacebbd
Connor Rhodes <connor@rhodes.contact>
2022-12-27 15:31:51 -0600

    added file and prompt

---
+++ python-for-non-programmers/3_numbers/1_experiments/calculator.py
@@ -0,0 +1,5 @@
+# (1) Ask the user to enter a number (x)
+# (2) Ask the user to enter a second number (y)
+# (3) Ask the user to enter "operation, either '+' or '*'
+# (4) Print either x+y or x*y, depending on the operation
+
