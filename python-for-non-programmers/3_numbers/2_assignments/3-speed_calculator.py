3de10f70c8201c768c1736c24321639faa84ed6e
Connor Rhodes <connor@rhodes.contact>
2022-12-29 15:28:59 -0600

    completed assignment

---
+++ python-for-non-programmers/3_numbers/2_assignments/3-speed_calculator.py
@@ -0,0 +1,7 @@
+distance = float(input("How many miles did you walk or run? "))
+
+minutes = float(input("How many minutes did that take? "))
+
+mph = distance / (minutes / 60)
+
+print(f"You were traveling {mph} mph!")
