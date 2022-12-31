0af7946bab4bebca108b5bf69ba6dca950ce2c4c
Connor Rhodes <connor@rhodes.contact>
2022-12-30 21:43:25 -0600

    completed assignment used whitespace to make type conversion more readable

--- python-for-non-programmers/4_strings-1/2_assignments/1-print-letter-of-name.py
+++ python-for-non-programmers/4_strings-1/2_assignments/1-print-letter-of-name.py
@@ -4,3 +4,13 @@
 # and assign that to a variable called index.
 # (You can assume, for now, that the index
 # is legal for this string.) Print the letter at that index.
+
+name = input("Enter your name: ")
+
+index = int(
+    input("*Print nth character* Enter n: ")
+    # subtract 1 to fix with python's indexing from 0
+    ) - 1
+
+print(name[index])
+
