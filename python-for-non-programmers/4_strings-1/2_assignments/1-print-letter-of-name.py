c1d155111d946491a43d7c6a366d070062aa59c5
Connor Rhodes <connor@rhodes.contact>
2022-12-30 21:27:36 -0600

    added file with prompt

---
+++ python-for-non-programmers/4_strings-1/2_assignments/1-print-letter-of-name.py
@@ -0,0 +1,6 @@
+# Ask the user to enter their name
+# and assign that to a variable called name.
+# Then ask the user to enter an integer,
+# and assign that to a variable called index.
+# (You can assume, for now, that the index
+# is legal for this string.) Print the letter at that index.
