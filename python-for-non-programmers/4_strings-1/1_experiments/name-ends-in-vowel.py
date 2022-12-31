c63522ac8f8e9860501d067fb217a05e4e624146
Connor Rhodes <connor@rhodes.contact>
2022-12-30 21:02:02 -0600

    updated script to use negative index

--- python-for-non-programmers/4_strings-1/1_experiments/name-ends-in-vowel.py
+++ python-for-non-programmers/4_strings-1/1_experiments/name-ends-in-vowel.py
@@ -2,7 +2,7 @@ vowels = ['a', 'e', 'i', 'o', 'u']
 
 name = input("Please enter a name: ")
 
-if name[len(name) - 1] in vowels:
+if name[-1] in vowels:
     print("That name ends with a vowel.")
 else:
     print("That name does not end with a vowel.")
