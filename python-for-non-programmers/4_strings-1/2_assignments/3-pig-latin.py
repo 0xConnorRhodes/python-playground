e768ac56e7173fafc630f0146c610524322c8093
Connor Rhodes <connor@rhodes.contact>
2023-01-02 11:35:46 -0600

    completed script

--- python-for-non-programmers/4_strings-1/2_assignments/3-pig-latin.py
+++ python-for-non-programmers/4_strings-1/2_assignments/3-pig-latin.py
@@ -1,2 +1,8 @@
 # Ask the user to enter a word, and assign it to the variable word.
 # Then print the translation of the word into Pig Latin.
+
+word = input("Please enter a word: ")
+
+pl_word = word[1::] + word[0] + 'ay'
+
+print(pl_word)
