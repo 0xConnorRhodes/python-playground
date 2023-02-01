695f1f159ea27ce77d119a739ad00c5b1365e107
Connor Rhodes <connor@rhodes.contact>
2023-02-01 16:35:39 -0600

    added assignment file with prompt

---
+++ python-for-non-programmers/5_methods/2_assignments/2-print-substring-range.py
@@ -0,0 +1,10 @@
+'''
+Ask the user to enter a string. Then ask the user to enter two numbers
+(as separate inputs). Print the string from the first number to the
+second number, assuming that both are legitimate indexes in the string.
+If the user gives non-numbers, then scold them. If the user gives
+indexes that are negative or too high, scold them. (Please don’t
+scold your users when it comes to real software!) So if the user
+enters abcdefg and the numbers 3 and 5, then we should see de in
+the output, since that’s from index 3 to index 5 (not including 5).
+'''
