a05f90c12142ff9cefa1e64aefb429bea5291af1
Connor Rhodes <connor@rhodes.contact>
2023-02-01 17:11:34 -0600

    completed assignment

--- python-for-non-programmers/5_methods/2_assignments/2-print-substring-range.py
+++ python-for-non-programmers/5_methods/2_assignments/2-print-substring-range.py
@@ -8,3 +8,33 @@ scold your users when it comes to real software!) So if the user
 enters abcdefg and the numbers 3 and 5, then we should see de in
 the output, since that’s from index 3 to index 5 (not including 5).
 '''
+
+string = input("Please enter a string: ")
+num1 = input("Enter the lower bound: ")
+
+if '-' in num1:
+    print("Sorry. This program does not accept negative indices.")
+elif not num1.isdigit():
+    print("Sorry. That is not a valid index")
+    exit(1)
+elif int(num1) < 1:
+    print("Sorry. This program only supports indices from one.")
+    exit(1)
+
+num1 = int(num1)
+
+num2 = input("Enter the upper bound: ")
+
+if not num2.isdigit():
+    print("Sorry. That is not a valid indes")
+    exit(1)
+elif int(num2) > len(string):
+    print("Sorry. That number is too high for this string.")
+    exit(1)
+elif int(num2) < num1:
+    print("Second index must be greater than the first.")
+    exit(1)
+
+num2 = int(num2)
+
+print(string[num1:num2])
