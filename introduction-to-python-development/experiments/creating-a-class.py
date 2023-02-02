e36542d9e7e060d14b0258bcc7dc1400f1ab1bbd
Connor Rhodes <connor@rhodes.contact>
2023-02-01 21:40:39 -0600

    experimented with class

--- introduction-to-python-development/experiments/creating-a-class.py
+++ introduction-to-python-development/experiments/creating-a-class.py
@@ -2,11 +2,20 @@
 
 class Car:
     """
-    Docstring describing the class
+    Car models a car with tires and an engine
     """
 
-    def __init__(self):
-        """
-        Docstring describing the method
-        """
-        pass
+    def __init__(self, engine, tires):
+        self.engine = engine
+        self.tires = tires
+
+    def drive(self):
+        if self.tires < 4:
+            print("CRASH")
+        elif self.engine == "v6":
+            print("vroooooom")
+        elif self.engine == "v8":
+            print("vrooooooooom")
+        elif self.engine == "v12":
+            print("vroooooooooooom")
+
