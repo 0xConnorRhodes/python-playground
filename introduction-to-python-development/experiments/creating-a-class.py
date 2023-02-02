f1f3d0399527ebd2ce89dfe9d40130fc56cf94de
Connor Rhodes <connor@rhodes.contact>
2023-02-01 20:25:52 -0600

    added initial structure

---
+++ introduction-to-python-development/experiments/creating-a-class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+class Car:
+    """
+    Docstring describing the class
+    """
+
+    def __init__(self):
+        """
+        Docstring describing the method
+        """
+        pass
