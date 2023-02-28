395ce3c911b78170ed73824b11d539b56474a456
Connor Rhodes <connor@rhodes.contact>
2023-02-28 04:24:10 +0000

    fixed setup.py to allow package install and wrote main function for cli tool

--- introduction-to-python-development/projects/pgbackup/setup.py
+++ introduction-to-python-development/projects/pgbackup/setup.py
@@ -5,12 +5,19 @@ with open('README.md', 'r') as f:
 
 setup(
     name='pgbackup',
-    version='0.1.0'
-    author='Connor Rhodes'
-    author_email='connor@rhodes.contact'
+    version='0.1.0',
+    author='Connor Rhodes',
+    author_email='connor@rhodes.contact',
     description='A utility for backing up PostgreSQL databases.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://raw.githubusercontent.com/0xConnorRhodes/python-playground/main/introduction-to-python-scripting/projects/pgbackup',
-    packages=find_packages('src')
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=['boto3'],
+    entry_points={
+        'console_scripts': [
+            'pgbackup=pgbackup.cli:main'
+        ],
+    }
 )
