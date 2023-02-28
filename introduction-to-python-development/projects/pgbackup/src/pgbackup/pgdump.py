3e66443c3e5d52bd4a9c8af66d02c4e0531a0355
Connor Rhodes <connor@rhodes.contact>
2023-02-28 04:30:42 +0000

    added function to name file using database name and optionally with a timestamp

--- introduction-to-python-development/projects/pgbackup/src/pgbackup/pgdump.py
+++ introduction-to-python-development/projects/pgbackup/src/pgbackup/pgdump.py
@@ -8,3 +8,10 @@ def dump(url):
         print(f"Error: {err}")
         sys.exit(1)
 
+def dump_file_name(url, timestamp=None):
+    db_name = url.split('/')[-1] # split on slashes and show the last field which will be the database name
+    db_name = db_name.split('?')[0] # show only first field split on '?' which will remove any SQL query syntax that may be present
+    if timestamp:
+        return f"{db_name}-{timestamp}.sql"
+    else:
+        return f"{db_name}.sql"
