5445e8e571c77eb75ff103f6d63235c1c623a2a8
Connor Rhodes <connor@rhodes.contact>
2023-03-12 14:11:12 -0500

    removed prompt comment

--- python-for-non-programmers/12_functions/assignments/1-maxnum.py
+++ python-for-non-programmers/12_functions/assignments/1-maxnum.py
@@ -1,5 +1,3 @@
-# Write a function, maxnum, that takes a single argument, a list of numbers. It returns the largest number in that list. (Do not use the built-in max function to do this!)
-
 def maxnum(numbers):
     numbers.sort(reverse=True)
     return numbers[0]
