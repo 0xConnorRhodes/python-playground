4ac0285b552d947af8707596fc257afb4298f970
Connor Rhodes <connor@rhodes.contact>
2023-03-12 10:40:41 -0500

    rewrote script to write to file instead of dictionary to account for repeated words

--- python-for-non-programmers/11_writing-to-files/assignments/1-multi-word-vowel-counter.py
+++ python-for-non-programmers/11_writing-to-files/assignments/1-multi-word-vowel-counter.py
@@ -1,18 +1,16 @@
-vowel_counts = {}
-while True:
-    sentence = input("Enter a sentence: ").strip()
-    if sentence == '':break
+with open('word-list.txt', 'w') as outfile:
+    while True:
+        sentence = input("Enter a sentence: ").strip()
 
-    words = sentence.split()
-
-    for one_word in words:
-        vowels = 0
-        for one_letter in one_word:
-            if one_letter.lower() in 'aeiou':
-                vowels += 1
-
-        vowel_counts[one_word] = vowels
-
-for word, count in vowel_counts.items():
-    print(f'{word}: {count}')
-
+        if sentence == '':
+            break
+    
+        words = sentence.split()
+    
+        for one_word in words:
+            vowels = 0
+            for one_letter in one_word:
+                if one_letter.lower() in 'aeiou':
+                    vowels += 1
+    
+            outfile.write(f'{one_word} {vowels}\n')
