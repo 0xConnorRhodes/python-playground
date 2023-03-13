51827bb54363f6fbb5fea4bcd8544142427b7e0e
Connor Rhodes <connor@rhodes.contact>
2023-03-13 16:42:47 -0500

    wrote function to calculate frequency of word length in specified file

---
+++ python-for-non-programmers/13_more-functions/assignments/2-wordslength.py
@@ -0,0 +1,12 @@
+# length: number_of_occurrances
+def wordslength(file):
+    dictionary = {}
+    for one_line in open(file):
+        for one_word in one_line.split():
+            key = str(len(one_word))
+            if key not in dictionary:
+                dictionary[key] = 1
+            else:
+                dictionary[key] += 1
+    return dictionary
+
