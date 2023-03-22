5d081afa32f0dd2a5a59cf64298584f7e21ce7b8
Connor Rhodes <connor@rhodes.contact>
2023-03-21 19:07:59 -0500

    added config.py with default environment values

---
+++ introduction-to-python-development/projects/flask-md-notes/config.py
@@ -0,0 +1,15 @@
+import os
+
+# pull default variables from os envirionment
+# and specify default values if unset in environment
+db_host = os.environ.get('DB_HOST', default='localhost')
+db_name = os.environ.get('DB_NAME', default='notes')
+db_user = os.environ.get('DB_USERNAME', default='db_user')
+db_user = os.environ.get('DB_PASSWORD', default='')
+db_port = os.environ.get('DB_PORT', default='5432')
+
+# remove warnings from output
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+SQLALCHEMY_DATABASE_URI = f'postgres://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
+
