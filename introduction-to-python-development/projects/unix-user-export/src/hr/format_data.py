9b29e0c932ecd753f92fbf7b21315f3f4399155c
Connor Rhodes <connor@rhodes.contact>
2023-03-31 18:43:08 -0500

    added function to print csv data to screen

--- introduction-to-python-development/projects/unix-user-export/src/hr/format_data.py
+++ introduction-to-python-development/projects/unix-user-export/src/hr/format_data.py
@@ -12,11 +12,17 @@ def write_json_file(export_file):
     with open(f'{export_file}.json', 'w') as file:
         json.dump(users, file, sort_keys=True, indent=4)
 
+def print_csv():
+    print('username,user id,home directory,shell')
+    for i in users:
+        row = f'{i["username"]},{i["user id"]},{i["home directory"]},{i["shell"]}'
+        print(row)
+
 
 def write_csv_file(export_file):
     with open(f'{export_file}.csv', 'w') as file:
         file.write('username,user id,home directory,shell\n')
 
         for i in users:
-            row = f'{i["username"]},{i["user id"]},{i["home directory"]},{i["shell"]},\n'
+            row = f'{i["username"]},{i["user id"]},{i["home directory"]},{i["shell"]}\n'
             file.write(row)
