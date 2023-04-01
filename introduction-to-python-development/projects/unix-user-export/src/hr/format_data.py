89f4908345cc8d228bb00c8409006799ebd1881c
Connor Rhodes <connor@rhodes.contact>
2023-03-31 19:49:29 -0500

    added logic to name filename correctly

--- introduction-to-python-development/projects/unix-user-export/src/hr/format_data.py
+++ introduction-to-python-development/projects/unix-user-export/src/hr/format_data.py
@@ -1,15 +1,19 @@
 import json
-import user  # imports user.py from the local directory
+import hr.users  # imports user.py from the local directory
 
-users = user.get_users_info()
+users = hr.users.get_users_info()
 
 
-def pretty_print_users():
-    return json.dumps(users, sort_keys=True, indent=4)
+def print_json():
+    print(json.dumps(users, sort_keys=True, indent=4))
 
 
 def write_json_file(export_file):
-    with open(f'{export_file}.json', 'w') as file:
+    # if the filename doesn't end in the file extension, add it automatically
+    if not export_file.split('.')[-1] == 'json':
+        export_file = export_file + '.json'
+
+    with open(f'{export_file}', 'w') as file:
         json.dump(users, file, sort_keys=True, indent=4)
 
 def print_csv():
@@ -20,7 +24,11 @@ def print_csv():
 
 
 def write_csv_file(export_file):
-    with open(f'{export_file}.csv', 'w') as file:
+    # if the filename doesn't end in the file extension, add it automatically
+    if not export_file.split('.')[-1] == 'csv':
+        export_file = export_file + '.csv'
+
+    with open(f'{export_file}', 'w') as file:
         file.write('username,user id,home directory,shell\n')
 
         for i in users:
