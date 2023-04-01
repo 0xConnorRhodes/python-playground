a6773c890247a1fd6ded77b30fc165baf3c74499
Connor Rhodes <connor@rhodes.contact>
2023-03-31 19:47:24 -0500

    added entrypoint to setup.py to create executable

--- introduction-to-python-development/projects/unix-user-export/setup.py
+++ introduction-to-python-development/projects/unix-user-export/setup.py
@@ -8,11 +8,14 @@ setup(
     version='0.1.0',
     author='Connor Rhodes',
     author_email='connor@rhodes.contact',
-    description-'Utility to export user information on a unix system',
+    description='Utility to export user information on a unix system',
     long_description=long_description,
     long_description_content_type='text/x-rst',
     url='https://raw.githubusercontent.com/0xConnorRhodes/python-playground/main/introduction-to-python-scripting/projects/unix-user-export',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     install_requires=[],
+    entry_points={
+        'console_scripts': 'hr=hr.cli:main'
+    }
 )
