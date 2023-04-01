48ffdad4dffcc82aa2f21d8e380d500c125788d1
Connor Rhodes <connor@rhodes.contact>
2023-03-31 19:49:11 -0500

    added main function to execute script based on parsed args

--- introduction-to-python-development/projects/unix-user-export/src/hr/cli.py
+++ introduction-to-python-development/projects/unix-user-export/src/hr/cli.py
@@ -1,13 +1,12 @@
-import sys
 from argparse import ArgumentParser, Namespace
 
 
 def create_parser():
-    parser = ArgumentParser
+    parser = ArgumentParser()
 
     parser.add_argument(
         '--format',
-        type=str.lower(),
+        type=str.lower,
         choices=['json', 'csv'],
         default='json',
         help='specify output format: json or csv (default: json)'
@@ -21,5 +20,20 @@ def create_parser():
 
     return parser
 
+
 def main():
+    import hr.format_data as format_data
+
+    args: Namespace = create_parser().parse_args()
+
+    if args.path:
+        if args.format == 'csv':
+            format_data.write_csv_file(args.path)
+        else:
+            format_data.write_json_file(args.path)
+    else:
+        if args.format == 'csv':
+            format_data.print_csv()
+        else:
+            format_data.print_json()
 
