95a5e314af517b47c40be73af126e6cce75cf908
Connor Rhodes <connor@rhodes.contact>
2023-04-08 19:41:23 +0000

    fixed poastgresql URI for newer version of SQLAlchemy

--- introduction-to-python-development/projects/flask-md-notes/config.py
+++ introduction-to-python-development/projects/flask-md-notes/config.py
@@ -11,5 +11,5 @@ db_port = os.environ.get('DB_PORT', default='5432')
 # remove warnings from output
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-SQLALCHEMY_DATABASE_URI = f'postgres://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
+SQLALCHEMY_DATABASE_URI = f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
 
