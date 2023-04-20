1f3eb4830de17dcb775b3d71e61d210f77250e59
Connor Rhodes <connor@rhodes.contact>
2023-04-20 10:31:24 -0500

    added functions challenge

--- certified-entry-level-python-programmer/Defining and Using Python Functions/testing-functions.py
+++ certified-entry-level-python-programmer/Defining and Using Python Functions/testing-functions.py
@@ -1,5 +1,10 @@
 # 1) Write a `split_name` function that takes a string and returns a dictionary with first_name and last_name
 
+def split_name(name):
+    first, last = name.split(maxsplit=1)
+    return {"first_name": first, "last_name": last}
+
+
 assert split_name("Kevin Bacon") == {
     "first_name": "Kevin",
     "last_name": "Bacon",
@@ -12,7 +17,15 @@ assert split_name("Victor Von Doom") == {
     "last_name": "Von Doom",
 }, f"Expected {{'first_name': 'Victor', 'last_name': 'Von Doom'}} but received {split_name('Victor Von Doom')}"
 
+
 # 3) Write an `is_palindrome` function to check if a string is a palindrome (reads the same from left-to-right and right-to-left)
+
+def is_palindrome(given):
+    given = str(given)
+    if given == given[::-1]:
+        return True
+    else:
+        return False
 
 
 assert is_palindrome("radar") == True, f"Expected True but got {is_palindrome('radar')}"
@@ -23,7 +36,12 @@ assert is_palindrome("stop") == False, f"Expected False but got {is_palindrome('
 assert is_palindrome(101) == True, f"Expected True but got {is_palindrome(101)}"
 assert is_palindrome(10) == False, f"Expected False but got {is_palindrome(10)}"
 
+
 # 5) Write a `build_list` function that takes an item and a number to include in a list
+
+def build_list(item, count=1):
+    return [item for _ in range(count)]
+
 
 assert build_list("Apple", 3) == [
     "Apple",
