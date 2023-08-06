7f2626db622da21826d521a1524554c3c1436558
Connor Rhodes <connor@rhodes.contact>
2023-08-06 08:13:45 -0500

    completed BMI calculator 2.0

--- 100-days-of-python/day-3_control-flow-and-logical-operators/exercise-2_BMI-calculator-2.0.py
+++ 100-days-of-python/day-3_control-flow-and-logical-operators/exercise-2_BMI-calculator-2.0.py
@@ -4,3 +4,18 @@ weight = float(input("enter your weight in kg: "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+
+BMI = round(weight / height ** 2)
+
+if BMI < 18.5:
+    BMI_interpretation = "are underweight"
+elif BMI < 25:
+    BMI_interpretation = "have a normal weight"
+elif BMI < 30:
+    BMI_interpretation = "are slightly overweight"
+elif BMI < 35:
+    BMI_interpretation = "are obese"
+else:
+    BMI_interpretation = "are clinically obese"
+
+print(f"Your BMI is {BMI}, you {BMI_interpretation}.")
