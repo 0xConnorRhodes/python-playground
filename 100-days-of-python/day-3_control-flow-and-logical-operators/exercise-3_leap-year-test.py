988a2621cffa44d3a5185da40712d2bfb23eac16
Connor Rhodes <connor@rhodes.contact>
2023-08-06 08:18:45 -0500

    added definition and testing comments

--- 100-days-of-python/day-3_control-flow-and-logical-operators/exercise-3_leap-year-test.py
+++ 100-days-of-python/day-3_control-flow-and-logical-operators/exercise-3_leap-year-test.py
@@ -5,3 +5,10 @@ year = int(input("Which year do you want to check? "))
 #Write your code below this line 👇
 
 #Write your code below this line 👇
+
+# a leap year is
+# on every year that is evenly divisible by 4
+# except every year that is evenly divisible by 100
+# unless the year is also evenly divisible by 400
+
+# for example: 2020 and 2024 are leap years. 2022 is not
