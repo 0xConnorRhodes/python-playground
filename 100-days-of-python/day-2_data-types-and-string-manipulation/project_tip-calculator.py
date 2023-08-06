5ffcf705d630a04609d5b119d271a7f04b1c6414
Connor Rhodes <connor@rhodes.contact>
2023-08-05 20:06:10 -0500

    completed tip calculator

---
+++ 100-days-of-python/day-2_data-types-and-string-manipulation/project_tip-calculator.py
@@ -0,0 +1,13 @@
+print("Welcome to the tip calculator.")
+
+bill = float(input("What was the total bill? $"))
+split_number = int(input("How many people to split the bill? "))
+bill_split = bill / split_number
+
+tip_pct = 0.01 * int(input("What percentage tip would you like to give? "))
+total_tip = bill * tip_pct
+tip_split = total_tip / split_number
+
+person_amt = round((bill_split + tip_split), 2)
+
+print(f"Each person should pay ${person_amt}")
