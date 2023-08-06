fe72178ed94e91eecf29faf855b188e2c7743bb0
Connor Rhodes <connor@rhodes.contact>
2023-08-05 20:20:48 -0500

    updated formatting to always show two decimal places

--- 100-days-of-python/day-2_data-types-and-string-manipulation/project_tip-calculator.py
+++ 100-days-of-python/day-2_data-types-and-string-manipulation/project_tip-calculator.py
@@ -8,6 +8,6 @@ tip_pct = 0.01 * int(input("What percentage tip would you like to give? "))
 total_tip = bill * tip_pct
 tip_split = total_tip / split_number
 
-person_amt = round((bill_split + tip_split), 2)
+person_amt = "{:.2f}".format(round((bill_split + tip_split), 2))
 
 print(f"Each person should pay ${person_amt}")
