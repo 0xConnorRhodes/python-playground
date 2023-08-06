a20658e8f51352d3e6564ff0281cc91d162d05d2
Connor Rhodes <connor@rhodes.contact>
2023-08-06 08:31:10 -0500

    completed leap year tester

--- 100-days-of-python/day-3_control-flow-and-logical-operators/exercise-3_leap-year-test.py
+++ 100-days-of-python/day-3_control-flow-and-logical-operators/exercise-3_leap-year-test.py
@@ -12,3 +12,20 @@ year = int(input("Which year do you want to check? "))
 # unless the year is also evenly divisible by 400
 
 # for example: 2020 and 2024 are leap years. 2022 is not
+
+# if {year} is evenly divisible by 4
+if year % 4 == 0:
+    # then if it is also evenly divisible by 100...
+    if year % 100 == 0:
+        # check if it's also evenly divisible by 400.
+        # if so, it is a leap year
+        if year % 400 == 0:
+            print(f"Leap year.")
+        # but if not, it isn't a leap year
+        else:
+            print(f"Not a leap year.")
+    # or if it isn't evently divisible by 100 at all
+    else:
+        print(f"Leap year.")
+else:
+    print(f"Not leap year.")
