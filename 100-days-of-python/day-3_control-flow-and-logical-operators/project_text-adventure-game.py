35d8161e4a07cc2f6e66372dcef3619334838bd3
Connor Rhodes <connor@rhodes.contact>
2023-08-07 21:23:03 -0500

    completed text adventure game

--- 100-days-of-python/day-3_control-flow-and-logical-operators/project_text-adventure-game.py
+++ 100-days-of-python/day-3_control-flow-and-logical-operators/project_text-adventure-game.py
@@ -20,3 +20,42 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 /______/______/______/______/______/______/______/______/______/______/______/
 *******************************************************************************
 ''')
+
+print("Welcome to Treasure Island. Your mission is to find the treasure.")
+
+choice1 = input("Choose which way to go: \"left\" or \"right\"\n").lower()
+
+if choice1 == "left":
+    print("You come to the door")
+elif choice1 == "right":
+    print("You fall into a hole.\nGame Over")
+    exit(0)
+else:
+    print("Sorry, that is not a valid choice")
+    exit(1)
+
+choice2 = input("You come to a river. Do you \"swim\" or \"wait\" for the ferry.\n").lower()
+
+if choice2 == "swim":
+    print("You are eaten by piranhas.\nGame Over")
+    exit(0)
+elif choice2 == "wait":
+    print("The ferry carries you across the river.")
+else:
+    print("Sorry, that is not a valid choice")
+    exit(1)
+
+choice3 = input("You come to three doors. One red, one yellow, and one blue. Which door do you open?\n").lower()
+
+if choice3 == "yellow":
+    print("You Win!")
+    exit(0)
+elif choice3 == "red":
+    print("You are burned by fire.\nGame Over")
+    exit(0)
+elif choice3 == "blue":
+    print("You are eaten by beasts.\nGame Over")
+    exit(0)
+else:
+    print("Sorry, that is not a valid choice")
+    exit(1)
