c6011924c7e4ad543146e5ae060d3868ef0d7d80
Connor Rhodes <connor@rhodes.contact>
2023-08-16 20:22:59 -0500

    completed rock paper scissors assignment, with error checking

--- 100-days-of-python/day-4_randomization-and-lists/project_rock-paper-scissors.py
+++ 100-days-of-python/day-4_randomization-and-lists/project_rock-paper-scissors.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -26,4 +28,36 @@ scissors = '''
 '''
 
 #Write your code below this line 👇
+
+choices = [rock, paper, scissors]
+
+player_choice = input("What do you choose? Type 0 for rock, 1 for paper, or 2 for scissors.\n")
+
+if player_choice not in ['0', '1', '2']:
+    print("You typed an invalid choice.")
+    exit(1)
+
+player_choice = int(player_choice)
+
+computer_choice = random.randint(0, 2)
+
+def print_choices():
+    print(f"You chose:\n{choices[player_choice]}")
+    print(f"Computer chose:\n{choices[computer_choice]}")
+
+if player_choice == computer_choice:
+    print_choices()
+    print("It's a tie.")
+elif player_choice == 2 and computer_choice == 0:
+    print_choices()
+    print("You lose.")
+elif computer_choice == 2 and player_choice == 0:
+    print_choices()
+    print("You win.")
+elif player_choice > computer_choice:
+    print_choices()
+    print("You win.")
+elif computer_choice > player_choice:
+    print_choices()
+    print("You lose.")
 
