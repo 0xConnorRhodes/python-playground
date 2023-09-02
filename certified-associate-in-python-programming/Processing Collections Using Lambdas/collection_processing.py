3e391bc659591506a88bc0044ba7721a9625afc3
Connor Rhodes <connor@rhodes.contact>
2023-09-02 15:22:51 -0500

    nested functions to filter and sort list to all persons under 70

--- certified-associate-in-python-programming/Processing Collections Using Lambdas/collection_processing.py
+++ certified-associate-in-python-programming/Processing Collections Using Lambdas/collection_processing.py
@@ -25,7 +25,9 @@ assert sorted_by_name == [
 # `<NAME> is <AGE> years old.` where the `<NAME>` and `<AGE>` values are from
 # the dictionaries.
 
-name_declarations = list(map(lambda d: f"{d['name']} is {d['age']} years old", sorted_by_name))
+name_declarations = list(
+    map((lambda d: f"{d['name']} is {d['age']} years old"), sorted_by_name)
+)
 
 assert name_declarations == [
     "Ariana Richards is 40 years old",
@@ -39,7 +41,9 @@ assert name_declarations == [
 # new list called `under_seventy` that only contains the dictionaries where the
 # 'age' key is less than 70, sorting the list by age.
 
-under_seventy = None
+under_seventy = sorted(
+    filter((lambda d: d['age'] < 70), sorted_by_name), key=(lambda d: d['age'])
+)
 
 assert under_seventy == [
     {"name": "Ariana Richards", "age": 40},
