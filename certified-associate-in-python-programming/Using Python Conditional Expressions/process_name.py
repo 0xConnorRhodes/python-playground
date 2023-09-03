6d7752c7cadb5b2ac4e17a14ab578971bc4a8f7e
Connor Rhodes <connor@rhodes.contact>
2023-09-03 10:41:25 -0500

    rewrite functions inside for loop using conditional expression and updated nested functions for lisp-like syntax

--- certified-associate-in-python-programming/Using Python Conditional Expressions/process_name.py
+++ certified-associate-in-python-programming/Using Python Conditional Expressions/process_name.py
@@ -1,11 +1,9 @@
 name = input("What is your first name? ")
 
 # 1) Call `print` with a different string using a single conditional expression
-print(
-    "Your name is as long or longer than the average first name in the United States"
-) if len(name) >= 6 else print(
-    "Your name is shorter than the average first name in the United States"
-)
+(print("Your name is as long or longer than the average first name in the United States")
+ if len(name) >= 6
+ else print("Your name is shorter than the average first name in the United States"))
 
 # 2) Set `message` using a single conditional expression
 message = (
@@ -18,7 +16,6 @@ print(message)
 
 # 3) Change the string passed to the `print` function using a conditional expression
 for letter in name:
-    if letter.lower() in ["a", "e", "i", "o", "u"]:
-        print(f"{letter} is a vowel")
-    else:
-        print(f"{letter} is a consonant")
+    (print(f"{letter} is a vowel")
+     if letter.lower() in ["a", "e", "i", "o", "u"]
+     else print(f"{letter} is a consonant"))
