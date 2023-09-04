5e5f12990496c0b32ecf575dd82111d8763c6be3
Connor Rhodes <connor@rhodes.contact>
2023-09-04 13:27:27 -0500

    added strhelpers.py with reverse function

---
+++ certified-associate-in-python-programming/Creating a Python Module/strhelpers.py
@@ -0,0 +1,11 @@
+def reverse(string):
+    index = 0
+    reversed_string = ''
+    for i in range(len(string)):
+        index -=1
+        reversed_string += input[index]
+    return reversed_string
+
+
+def shuffle():
+    pass
